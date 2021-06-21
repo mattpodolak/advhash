72b6d0388a166db9524965fb2976717a00717f4d
Matthew Podolak <mpodola2@gmail.com>
2021-06-21 08:27:27 -0400

    added image-tensor conversion functions

--- advhash/utils/convert.py
+++ advhash/utils/convert.py
@@ -1,9 +1,15 @@
+from PIL.Image import Image
 import torch
+import numpy as np
 
-def rgb2luma(x): # works, is differentiable
-    """
-        x: float tensor
-        returns float tensor
+def rgb2luma(x):
+    """Convert a tensor from RGB to Luma
+
+    Args:
+        x: PyTorch tensor with shape (:, :, 3)
+    
+    Returns:
+        PyTorch tensor with shape (:, :, 1)
     """
     mod = torch.ones(x.shape).cuda()
     add = torch.zeros(x.shape).cuda()
@@ -13,3 +19,33 @@ def rgb2luma(x): # works, is differentiable
     mod[:,:,2] = torch.mul(mod[:,:,2],7471)
     mod = torch.div(mod, pow(2, 16))
     return torch.sum(torch.add(torch.mul(x, mod),add), dim=-1)
+
+def tensor2image(x):
+    """Convert a tensor into an Image
+
+    Args:
+        x: PyTorch tensor
+
+    Returns:
+        Pillow Image
+
+    """
+    x = torch.clip(torch.round(x), 0, 255).cpu().detach().numpy().astype('uint8')
+    return Image.fromarray(x)
+
+def image2tensor(x, device=None):
+    """Convert an Image to a tensor
+
+    Args:
+        x: Pillow Image
+        device (optional): PyTorch device for the tensor - defaults
+        to CPU
+
+    Returns:
+        PyTorch float32 tensor on the chosen device
+
+    """
+    if device is None:
+        device = torch.device('cpu')
+
+    return torch.tensor(np.asarray(x).astype('float32')).to(device)
