c22d5400352fa2cebe86a4cd2b0a7492ae3924e6
Matthew Podolak <mpodola2@gmail.com>
2021-06-21 15:09:04 -0400

    l2 attack and base attack

--- advhash/attack/base.py
+++ advhash/attack/base.py
@@ -1,13 +1,103 @@
+import abc
+import torch
+from advhash import hashes
+from tqdm import trange
+from advhash.utils.constrain import box, box_conv
+from advhash.utils.compare import avg_diff
+from advhash.attack.hinge import hamming_dist
 
 class Attack(object):
-  """
-    Base class for adversarial attacks 
-  """
+    """Base class for adversarial attacks
 
-  def __init__(self, device= 'cuda'):
-    if device == 'cuda':
-      print()
-    elif device == 'cpu':
-      print()
-    else:
-      raise ValueError('device should equal cuda or cpu')
+    Args:
+        hash_fn: String or Hash function. Defines what hashing function to
+        perform an attack against.
+        hash_size (optional): Integer. Size of hash created by the hashing function 
+        - default value is 16.
+        split_point (optional): String. Defines what interior function to target as
+        part of the attack - default value is None.
+        device: String. Defines the device to store PyTorch tensors on.
+
+    Raises:
+        ValueError: For invalid arguments
+    """
+
+    def __init__(self, hash_fn, hash_size=16, split_point=None, device= 'cuda'):
+        if device == "cuda":
+            if torch.cuda.is_available():
+                self.device = torch.device('cuda')
+            else:
+                raise ValueError('Cuda is not available on this computer')
+        elif device == 'cpu':
+            self.device = torch.device('cpu')
+        else:
+            raise ValueError(f'device should equal cuda or cpu, instead got {device}')
+
+        self.metrics = {'distance': [], 'loss': [], 'avg_diff':[]}
+        self.hash = self._get_hash(hash_fn, hash_size, split_point)
+
+    def _get_hash(hash_fn, hash_size, split_point=None):
+        hash_cls = hashes.get(hash_fn)
+        if(hash_cls is None):
+            raise ValueError(f'Failed to initialize hash function for {hash_fn}')
+        return hash_cls(split_point=split_point, hash_size=hash_size)
+
+    def _update_metrics(self, **kwargs):
+        for metric in kwargs:
+            if metric in self.metrics:
+                self.metrics[metric] = kwargs[metric]
+
+    @abc.abstractmethod
+    def _loss_fn(self):
+        """Attack loss function for optimization
+
+        Returns:
+            PyTorch tensor with the loss value
+        """
+
+        return None
+
+    def attack(self, X, Y, epochs=1000, lr=0.005, betas=(0.1, 0.1), target_dist=0, **kwargs):
+        """Perform an adversarial collision attack on an image hashing function
+
+        Args:
+            X: PyTorch tensor - source image 
+            Y: PyTorch tensor - target image
+            epochs (optional): number of iterations for generating the image - defaults to 1000
+            lr (optional): learning rate for adam optimizer - defaults to 0.005
+            betas (optional): tuple of betas for the adam optimizer - defaults to (0.1, 0.1)
+            target_dist (optional): exit early if the hamming distance <= target_dist - defaults to 0
+
+        Returns:
+            PyTorch tensor of adversarial image
+
+        Raise:
+            ValueError: for invalid arguments
+
+        """
+        if type(X) != torch.tensor or type(Y) != torch.tensor:
+            raise ValueError("Expected torch.tensor for X and Y")
+
+        if X.device != self.device or Y.device != self.device:
+            raise ValueError(f"Expected X and Y to be on {self.device}")
+
+        w = box_conv(X)
+        X_orig = box(w)
+        Y_hash = self.hash.full_hash(Y)
+        w.requires_grad = True
+        self.opt = torch.optim.Adam([w], lr=lr, betas=betas)
+        with trange(epochs) as t:
+            for epoch in t:
+                X_adv = box(w)
+                self.opt.zero_grad()
+                loss = self._loss_fn(X_adv, X_orig, Y, **kwargs)
+                X_hash = self.hash.full_hash(X_adv)
+                dist = hamming_dist(X_hash, Y_hash)
+                diff = avg_diff(X_orig, X_adv)
+                self._update_metrics(distance=dist.item(), loss=loss.item(), avg_diff=diff.item())
+                t.set_description(f'Epoch {epoch+1}: loss={loss:.4f} dist={dist} diff={100*diff:.2f}% ')
+                if(dist <= target_dist):
+                    break
+                loss.backward()
+                self.opt.step()
+        return box(w)
--- advhash/attack/l2.py
+++ advhash/attack/l2.py
@@ -1,169 +1,47 @@
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-import imagehash
-import torch
-from tqdm.notebook import trange
+from advhash.attack.base import Attack
 import os
+import torch
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = "True" # set env variable to deal with matplotlib and torch issue
 
-def rgb2luma(x): # works, is differentiable
-    """
-        x: float tensor
-        returns float tensor
-    """
-    mod = torch.ones(x.shape).cuda()
-    add = torch.zeros(x.shape).cuda()
-    add[:,:,2] = (torch.ones(add[:,:,2].shape).cuda() * 32768) / pow(2, 16)
-    mod[:,:,0] = torch.mul(mod[:,:,0],19595)
-    mod[:,:,1] = torch.mul(mod[:,:,1],38470) 
-    mod[:,:,2] = torch.mul(mod[:,:,2],7471)
-    mod = torch.div(mod, pow(2, 16))
-    return torch.sum(torch.add(torch.mul(x, mod),add), dim=-1)
+class L2Attack(Attack):
+    """Implementation of an L2 norm based attack.
 
-def box(x, boxmin=0,boxmax=255):
-    """
-        Convert [-inf,inf] to [min, max] using tanh
-    """
-    x = torch.tanh(x) # map [-inf,inf] to [-1,1]
-    grad = 1 - torch.square(x)
-    boxmul = (boxmax - boxmin) / 2.
-    boxplus = (boxmin + boxmax) / 2.
-    x = x*boxmul+boxplus # map [-1, +1] to [min, max]
-    grad=grad*boxmul
-    return x
+    Reference:
+        Dolhansky, B., & Canton-Ferrer, C. (2020, November). Adversarial collision attacks 
+        on image hashing functions. https://arxiv.org/pdf/2011.09473.pdf
 
-def box_conv(x, boxmin=0,boxmax=255):
-    """
-        Convert [min, max] to [-inf,inf] using arctanh
-    """
-    boxmul = (boxmax - boxmin) / 2.
-    boxplus = (boxmin + boxmax) / 2.
-    x = (x - boxplus) / boxmul # map [min, max] to [-1, +1]
-    return torch.arctanh(x*0.999999) # map [-1,1] to [-inf,inf]
-
-def dist_check(X, Y):
-    X = Image.fromarray((X).cpu().detach().numpy().astype('uint8'))
-    Y = Image.fromarray((Y).cpu().detach().numpy().astype('uint8'))
-    X=torch.tensor(imagehash.dhash(X, hash_size=16).hash, dtype=torch.float32).cuda()
-    Y=torch.tensor(imagehash.dhash(Y, hash_size=16).hash, dtype=torch.float32).cuda()
-    return torch.sum(torch.abs(X - Y))
-
-def sinc(x):
-    if (x == 0.0):
-        return torch.tensor(1.0)
-    x = x * np.pi
-    return np.sin(x) / x
-
-def lanczos3(x):
-    if(-3.0 <= x and x < 3.0):
-        return sinc(x)*sinc(x/3.0)
-    else:
-        return torch.tensor(0.0)
     
-def precompute_coeffs(inSize, outSize):
-    filterscale = scale = inSize / outSize
-    filterscale = max(1, filterscale)
-    support = 3 * filterscale # 3 for lanczos kernel size of 3
-    ksize = int(np.ceil(support) * 2 + 1)
-    kk = torch.zeros((outSize, inSize)).cuda()
-    for i in range(outSize):
-        center = (i+0.5)*filterscale
-        ww = 0.0
-        ss = 1.0 / filterscale
-        _min = max(0, int(center-support+0.5))
-        _max = min(inSize, int(center+support+0.5))
-        for j in range(_min, _max):
-            kk[i][j] = lanczos3((j-center+0.5)*ss)
-        kk[i] = torch.div(kk[i], torch.sum(kk[i]))
-    return kk
+    Example:
+        ```
+        from advhash.attack.l2 import L2Attack
+        import torch
+        from PIL import Image
 
-def resample(imIn, kkx, kky, xsize=17, ysize=16):
-    im_y, im_x = imIn.shape
-    if(im_x != xsize):
-        imIn = torch.clip(torch.matmul(imIn, torch.transpose(kkx, 0, 1)), 0, 255)
-    if(im_y != ysize):
-        imIn = torch.clip(torch.matmul(kky, imIn), 0, 255)
-    return imIn
+        target = Image.open('forest.jpg')
+        source = Image.open('cat.jpg')
 
-def lanczos_resize(X, xsize=17, ysize=16):
-    im_y, im_x = X.shape
-    kkx = precompute_coeffs(im_x, xsize)
-    kky = precompute_coeffs(im_y, ysize)
-    return resample(X, kkx, kky)
+        l2 = L2Attack(hash_fn='dhash', split_point='resize')
 
-def actual_hash_fn(X):
-    X = rgb2luma(X)
-    X = lanczos_resize(X)
-    return (X[:, 1:] > X[:, :-1]).type(torch.float32)
+        im_adv = l2.attack(target, source)
 
-def short_dist_check(X, Y):
-    X_hash=actual_hash_fn(X)
-    Y_hash=actual_hash_fn(Y)
-    return torch.sum(torch.abs(X_hash - Y_hash))
+        ```
 
-def loss_hash_fn(X):
-    X = rgb2luma(X)
-    X = lanczos_resize(X)
-    return X #X[:, 1:] - X[:, :-1]
+    """
+    def __init__(self, hash_fn, split_point, hash_size=16, device='cuda'):
+        super().__init__(hash_fn, hash_size, split_point, device)
 
-def content_loss(X_orig, X_pert):
-    return torch.mean(torch.abs(X_pert - X_orig)/X_orig)
+    def _loss_fn(self, X_adv, X_orig, Y_img, c=0.001):
+        """
+            Loss function used during the creation of the adversarial image
 
-def torch_norm(X): # works is differentiable
-    avoid_zero_div = torch.tensor(1e-12)
-    return torch.sqrt(torch.maximum(torch.sum(X**2), avoid_zero_div))
+            Args:
+                X_adv: PyTorch tensor - adversarial image
+                X_orig: PyTorch tensor - unperturbed image
+                Y_img: PyTorch tensor - target image
+                c (optional): content loss parameter - defaults to 0.001
 
-def loss_fn(X, X_orig, kkx, kky, Y, c=0.001):
-    x = rgb2luma(X)
-    X_hash = resample(x, kkx, kky)
-    return torch.norm(X_hash-Y, 2) + c*torch.norm(X-X_orig, 2)
-
-def train_fn(X, Y, boxmin=0, boxmax=255, epochs=1000, lr=5, c=0.001):
-    metrics = {'distance': [], 'loss': [], 'content_loss':[]}
-    w = box_conv(X, boxmin, boxmax)
-    X = box(w, boxmin, boxmax)
-    Y_hash = loss_hash_fn(Y)
-    w.requires_grad = True
-    im_y, im_x, _ = X.shape
-    kkx = precompute_coeffs(im_x, 17)
-    kky = precompute_coeffs(im_y, 16)
-    opt = torch.optim.Adam([w], lr=lr, betas=(0.1,0.1))
-    with trange(epochs) as t:
-        for epoch in t:
-            im_adv = box(w, boxmin, boxmax)
-            opt.zero_grad()
-            loss = loss_fn(im_adv, X, kkx, kky, Y_hash, c)
-            dist = dist_check(im_adv, Y)
-            c_loss = content_loss(X, im_adv)
-            metrics['distance'].append(dist.item())
-            metrics['loss'].append(loss.item())
-            metrics['content_loss'].append(c_loss.item())
-            t.set_description(f'Epoch {epoch+1}: loss={loss:.4f} dist={dist} c_loss={100*c_loss:.2f}% ')
-            if(dist == 0):
-                break
-            loss.backward()
-            opt.step()
-
-    return box(w, boxmin, boxmax), metrics
-
-def quick_plot(X):
-    plt.imshow(X.cpu().detach().numpy(), cmap="gray")
-
-Y_img = Image.open('./img/forest.jpg')
-Y = torch.tensor(np.asarray(Y_img).astype('float32')).cuda()
-
-X_img = Image.open('./img/cat.jpg')
-X = torch.tensor(np.asarray(X_img).astype('float32')).cuda()
-
-im_adv, metrics = train_fn(X, Y, 0, 255, lr=0.005, epochs=1000, c=0.001)
-
-quick_plot(im_adv/255)
-
-convertedAdv = torch.clip(torch.round(im_adv), 0, 255).cpu().detach().numpy().astype('uint8')
-
-testRaw = Image.fromarray(convertedAdv)
-hashRaw = torch.tensor(imagehash.dhash(testRaw, hash_size=16).hash, dtype=torch.float32).cuda()
-hashTarget = torch.tensor(imagehash.dhash(Y_img, hash_size=16).hash, dtype=torch.float32).cuda()
-
-torch.sum(torch.abs(hashRaw - hashTarget))
+        """
+        X_hash = self.hash.partial_hash(X_adv)
+        Y_hash = self.hash.partial_hash(Y_img)
+        return torch.norm(X_hash-Y_hash, 2) + c*torch.norm(X_adv-X_orig, 2)
